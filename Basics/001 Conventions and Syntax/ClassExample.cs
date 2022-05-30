5a35aaf7c6a56a90fa6dc1a044b1d2bd201b36e9
Cossette Gomez <77295395+CossetteG@users.noreply.github.com>
2022-05-30 17:30:20 -0600

    My Laundry Class- thought process is on my C# Playground file

--- Basics/001 Conventions and Syntax/ClassExample.cs
+++ Basics/001 Conventions and Syntax/ClassExample.cs
@@ -1,6 +1,6 @@
 //Use this file to store your work to demonstrate your understanding of operators
 //Make sure your work is different from the instructors examples or your peers
-
+using System;
 public class ClassExample
 {
     
@@ -9,13 +9,16 @@ public class ClassExample
 public class ClassWithVariables
 {
     public int number = 2;
-    public string userName = "Bob";
+    public string userName = "Pearl";
+    public int value = 4;
+
 }
 
 public class ClassWithFunctions
 {
-    public void RunFunction()
+    public void Main()
     {
         Console.WriteLine("Running Function");
+        Console.WriteLine(value);
     }
 }   
--- Basics/001 Conventions and Syntax/MyClass.cs
+++ Basics/001 Conventions and Syntax/MyClass.cs
@@ -1,9 +1,68 @@
-//Use this file to store your work to demonstrate your understanding of operators
-//Make sure your work is different from the instructors examples or your peers
+using System;
 
-//Based on the HelloWorld File, turn this into a class.
 
-//Write a console program with a program class, and Main Function
-//Add another class with any name, add variables 
-//Using the new keyword create several objects
-//Console writeline the Objects variables
+public class Laundry
+{
+    public int shirts = 20;
+    public int pants = 7;
+    public int dresses = 14;
+    public long hangars = 50000000000L;
+
+    public string comment= "Time to do laundry";
+
+    public int clothesInTheWasher = 0;
+    public int clothesInTheDryer = 0;
+
+    public void Main()
+    {
+        Console.WriteLine(comment);
+
+        //Laundry Switch 1
+        clothesInTheWasher += shirts;
+        shirts -= shirts;
+        Console.WriteLine("Shirts are in the wash.");
+		
+        Console.WriteLine("   There are " + clothesInTheWasher+" items in the washer"); 
+        Console.WriteLine("   There are "+ clothesInTheDryer+ " items in the dryer");
+        
+        //Laundry Switch 2
+        clothesInTheDryer += clothesInTheWasher;
+        clothesInTheWasher -= (clothesInTheWasher);
+        clothesInTheWasher += (dresses);
+        dresses -= dresses;
+        Console.WriteLine("Shirts are drying, dresses are washing");
+
+        Console.WriteLine("   There are " + clothesInTheWasher+" items in the washer"); 
+        Console.WriteLine("   There are "+ clothesInTheDryer+ " items in the dryer");
+
+        //Laundry Switch 3
+        shirts += clothesInTheDryer;
+        clothesInTheDryer -= clothesInTheDryer;
+        clothesInTheDryer += clothesInTheWasher;
+        clothesInTheWasher -= (clothesInTheWasher);
+        clothesInTheWasher += pants;
+        Console.WriteLine("Shirts are clean, dresses are drying, pants are washing");
+
+        Console.WriteLine("   There are " + clothesInTheWasher+" items in the washer"); 
+        Console.WriteLine("   There are "+ clothesInTheDryer+ " items in the dryer");
+
+        //Laundry Switch 4
+        dresses += clothesInTheDryer;
+        clothesInTheDryer -= clothesInTheDryer;
+        clothesInTheDryer += clothesInTheWasher;
+        clothesInTheWasher -= (clothesInTheWasher);
+        Console.WriteLine("Dresses are clean, pants are drying");
+        Console.WriteLine("   There are " + clothesInTheWasher+" items in the washer"); 
+        Console.WriteLine("   There are "+ clothesInTheDryer+ " items in the dryer");
+
+        //Laundry Switch 5
+        pants += clothesInTheDryer;
+        clothesInTheDryer -= clothesInTheDryer;
+        Console.WriteLine("Pants are clean");
+
+        Console.WriteLine("   There are " + clothesInTheWasher+" items in the washer"); 
+        Console.WriteLine("   There are "+ clothesInTheDryer+ " items in the dryer");
+
+        Console.WriteLine("Now go fold your laundry");
+    }
+}
